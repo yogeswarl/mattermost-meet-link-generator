daaff63a81f3ce199ee480b8f1c8112f11a6fc1e
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2024-07-27 15:41:13 -0400

    initial commit Convert code to Express

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const { authenticate } = require('@google-cloud/local-auth');
+const { SpacesServiceClient } = require('@google-apps/meet').v2;
+const { auth } = require('google-auth-library');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/meetings.space.created'];
+
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return auth.fromJSON(credentials);
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Creates a new meeting space.
+ * @param {OAuth2Client} authClient An authorized OAuth2 client.
+ */
+async function createSpace(authClient) {
+  const meetClient = new SpacesServiceClient({
+    authClient: authClient
+  });
+  // Construct request
+  const request = {
+  };
+
+  // Run request
+  const response = await meetClient.createSpace(request);
+  return response[0].meetingUri;
+}
+
+app.get('/create-space', async (req, res) => {
+  
+  try {
+    const authClient = await authorize();
+    const meetingUri = await createSpace(authClient);
+    res.json( {"response_type":"in_channel","text":meetingUri});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating meeting space');
+  }
+});
+
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
