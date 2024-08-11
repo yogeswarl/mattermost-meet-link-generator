eb8510eb62ebd0cd056702605dc3528e0e83cf7e
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2024-08-11 11:08:10 -0400

    create nodeJs layering for Node_Modules

--- bundle.sh
+++ bundle.sh
@@ -2,8 +2,10 @@
 
 # Define the name of the zip file
 ZIP_FILE="bundle.zip"
-
+NODE_JS_FOLDER="nodejs" 
 # Create the zip file excluding node_modules and .vscode directories
-zip -r $ZIP_FILE . -x "node_modules/*" ".vscode/*" ".git/*" "credentials_org.json" "bundle.sh"
+zip -r $ZIP_FILE . -x "node_modules/*" ".vscode/*" ".git/*" "credentials_org.json" "bundle.sh" "package.json" "package-lock.json"
 
 echo "Files have been bundled into $ZIP_FILE, excluding node_modules and .vscode directories."
+
+zip -r $NODE_JS_FOLDER "node_modules/*" "package.json" "package-lock.json"
